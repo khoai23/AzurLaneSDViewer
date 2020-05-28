31482f8004caa4c726c1d4e889186ce6f7aa392f
citronous <alg.4chan@gmail.com>
2020-05-28 19:00:39 +0800

    Reupdate page build

--- js/main.js
+++ js/main.js
@@ -320,7 +320,6 @@ function onChangeLog(){
     //Grants read-only access to public information (includes public user profile info, public repository info, and gists)
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             cb(JSON.parse(xobj.response));
           }
     };
